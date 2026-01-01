1f6b4041970ae56544ed45e6f69a7af409b6efd2
zahra-mehdilou <mehdilouzahra@gmail.com>
2026-01-02 00:02:38 +0330

    Add files via upload

---
+++ Bitcoin.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+response = requests.get(url)
+data = response.json()
+price = data["bitcoin"]["usd"]
+print("current price is:", price)
+if price<100000:
+    print("Time to buy")
+    type(price)
+
