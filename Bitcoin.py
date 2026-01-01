954ec12065a947d9a6e2049cbec8e4e57e647258
zahra-mehdilou <mehdilouzahra@gmail.com>
2026-01-02 00:32:38 +0330

    Update Bitcoin.py

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,9 +1,15 @@
 import requests
+import time
 
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
-response = requests.get(url)
-data = response.json()
-price = data["bitcoin"]["usd"]
-print("current price is:", price)
-if price<100000:
-    print("Time to buy")
+while True:
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    response = requests.get(url)
+    data = response.json()
+    price = float(data["bitcoin"]["usd"])
+    print("current price is:", price)
+    if price<950000:
+        print("Time to buy")
+    else:
+        print("Wait!")
+    time.sleep(600)
+
