93a1a95fcb31a377d9c9d1949a16349e3d0242d0
zahra-mehdilou <mehdilouzahra@gmail.com>
2026-01-02 00:37:27 +0330

    Update Bitcoin.py

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,13 +1,13 @@
 import requests
 import time
 
-while True:
+while price<95000:
     url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
     response = requests.get(url)
     data = response.json()
     price = float(data["bitcoin"]["usd"])
     print("current price is:", price)
-    if price<950000:
+    if price<800000:
         print("Time to buy")
     else:
         print("Wait!")
